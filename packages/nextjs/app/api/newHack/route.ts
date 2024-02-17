6268e523619cc2403621c5ef725266b0507efb10
Carlos Di Matteo <carlosdima97@gmail.com>
2024-02-17 15:42:25 -0500

    implement metrics in evaluation creation

--- packages/nextjs/app/api/newHack/route.ts
+++ packages/nextjs/app/api/newHack/route.ts
@@ -1,140 +1,196 @@
 import { NextResponse } from "next/server";
+import { createChatEngine } from "../chat/engine";
+import { storeEvaluationByProject } from "../metrics/evaluation";
+import { storePrompt } from "../metrics/prompt";
+import { storeUsageByEmbeddingId } from "../metrics/usage";
+// Assumed environme
+import { Document, MongoDBAtlasVectorSearch, VectorStoreIndex, storageContextFromDefaults } from "llamaindex";
+import { OpenAI, serviceContextFromDefaults } from "llamaindex";
+import { Db, MongoClient } from "mongodb";
+import { Evaluation } from "~~/app/hackathon";
 // Assuming we've defined or imported types for the Hackathon Application
 //
-
 import type { HackathonEntry } from "~~/types/dbSchema";
 
-// Assumed environme
-import { MongoDBAtlasVectorSearch, VectorStoreIndex, storageContextFromDefaults, Document } from "llamaindex";
-import { MongoClient } from "mongodb";
-
-import { ChatMessage, MessageContent, OpenAI, serviceContextFromDefaults } from "llamaindex";
-import { createChatEngine } from "../chat/engine";
-
-const url = 'mongodb+srv://At0x:r8MzJR2r4A1xlMOA@cluster1.upfglfg.mongodb.net/?retryWrites=true&w=majority'
+const url = "mongodb+srv://At0x:r8MzJR2r4A1xlMOA@cluster1.upfglfg.mongodb.net/?retryWrites=true&w=majority";
 
 const client = new MongoClient(url);
 await client.connect();
 // Database Name
 
 async function llamaindex(payload: string, id: string) {
-    const vectorStore = new MongoDBAtlasVectorSearch({
-        mongodbClient: client,
-        dbName: "aiUniverse",
-        collectionName: "hackerIndex", // this is where your embeddings will be stored
-        indexName: "hacker_index", // this is the name of the index you will need to create
-    });
+ const vectorStore = new MongoDBAtlasVectorSearch({
+  mongodbClient: client,
+  dbName: "aiUniverse",
+  collectionName: "hackerIndex", // this is where your embeddings will be stored
+  indexName: "hacker_index", // this is the name of the index you will need to create
+ });
 
-    // now create an index from all the Documents and store them in Atlas
-    const storageContext = await storageContextFromDefaults({ vectorStore });
+ // now create an index from all the Documents and store them in Atlas
+ const storageContext = await storageContextFromDefaults({ vectorStore });
 
-    const essay = payload;
+ const essay = payload;
 
+ // Create Document object with essay
+ const document = new Document({ text: essay, id_: id });
+ console.log({ document });
+ // Split text and create embeddings. Store them in a VectorStoreIndex
+ const result = await VectorStoreIndex.fromDocuments([document], { storageContext });
+ const embeddingResults = await result.getNodeEmbeddingResults([document]);
+ console.log({ result, embeddingResults });
+ const db = client.db("aiUniverse"); // Connect to the database
+ const hackIndex = db.collection("hackerIndex");
 
-    // Create Document object with essay
-    const document = new Document({ text: essay, id_: id });
+ const embedding = await hackIndex.findOne({ "metadata.doc_id": id });
 
-    // Split text and create embeddings. Store them in a VectorStoreIndex
-    await VectorStoreIndex.fromDocuments([document], { storageContext });
+ console.log({ embeddingId: embedding?.id });
+ console.log(`Successfully created embeddings in the MongoDB collection`);
+ return { embeddingId: embedding?.id as string, result: embeddingResults };
+}
 
-    console.log(
-        `Successfully created embeddings in the MongoDB collection`,
-    );
+async function runLlamaAndStore(
+ db: Db,
+ hackathonApp: any,
+ enhancedProposal: string,
+ usedEmbeddingIds: string[],
+ promptMessages: any,
+ promptResponse: any,
+ evaluation: Evaluation,
+) {
+ const projectId = hackathonApp.projectId || hackathonApp.id;
+ const { embeddingId } = await llamaindex(JSON.stringify(enhancedProposal + hackathonApp), projectId); //should we modify this id?
+ // store in DB
+ const promptResult = await storePrompt(
+  db,
+  hackathonApp,
+  promptMessages,
+  embeddingId,
+  usedEmbeddingIds,
+  promptResponse,
+ );
+ const usageResult = await storeUsageByEmbeddingId(db, projectId, embeddingId, usedEmbeddingIds);
+ const evaluationResult = await storeEvaluationByProject(db, projectId, usedEmbeddingIds, embeddingId, evaluation);
+ return {
+  promptResult,
+  usageResult,
+  evaluationResult,
+ };
 }
 
 // Revised function suited for hackathon application data
 async function generateHackathonProposal(hackathonApp: HackathonEntry) {
-    const messages: any[] = [
-        {
-            role: "system",
-            content: `You are an AI consultant specializing in hackathon project conceptualization. Given a project name, problem statement, solution description, and technology stack, Ponder on the different aspects of the presented project and give a score for each domain. Use the evaluationRemarks to appraise the projects and compare them to each other. Reply in JSON format using the Eval type.`,
-        },
-        {
-            role: "assistant",
-            content: `
+ const messages: any[] = [
+  {
+   role: "system",
+   content: `You are an AI consultant specializing in hackathon project conceptualization. Given a project name, problem statement, solution description, and technology stack, Ponder on the different aspects of the presented project and give a score for each domain. Use the evaluationRemarks to appraise the projects and compare them to each other. Reply in JSON format using the Eval type.`,
+  },
+  {
+   role: "assistant",
+   content: `
               type Eval = {
          coherenceScore: number;
-         feasabilityScore: number;
+         feasibilityScore: number;
          innovationScore: number;
          funScore: number;
          evaluationRemarks: string;
                 }
             `,
-        },
-        {
-            role: "user",
-            content: `Review the hackathon entry, assign scores and provide evaluation remarks.
+  },
+  {
+   role: "user",
+   content: `Review the hackathon entry, assign scores and provide evaluation remarks.
   projectId: ${hackathonApp.projectId};
   projectName: ${hackathonApp.hack.projectName};
   problemStatement: ${hackathonApp.hack.problemStatement}
   solutionDescription: ${hackathonApp.hack.solutionDescription}
   technologyStack: ${hackathonApp.hack.technologyStack.join(", ")}
             `,
-        },
-    ];
+  },
+ ];
 
-    const llm = new OpenAI({
-        model: (process.env.MODEL as any) ?? "gpt-4-0125-preview",
-        maxTokens: 512,
-        additionalChatOptions: { response_format: { type: "json_object" } },
-    });
+ const llm = new OpenAI({
+  model: (process.env.MODEL as any) ?? "gpt-4-0125-preview",
+  maxTokens: 512,
+  additionalChatOptions: { response_format: { type: "json_object" } },
+ });
 
-    const serviceContext = serviceContextFromDefaults({
-        llm,
-        chunkSize: 512,
-        chunkOverlap: 20,
-    });
+ const serviceContext = serviceContextFromDefaults({
+  llm,
+  chunkSize: 512,
+  chunkOverlap: 20,
+ });
 
+ const chatEngine = await createChatEngine(serviceContext);
+ if (!chatEngine) {
+  throw new Error("datasource is required in the request body");
+ }
 
-    const chatEngine = await createChatEngine(
-        serviceContext,
-    );
-    if (!chatEngine) return NextResponse.json({ error: "datasource is required in the request body" }, { status: 400 });
+ // Convert message content from Vercel/AI format to LlamaIndex/OpenAI format
 
-    // Convert message content from Vercel/AI format to LlamaIndex/OpenAI format
+ const response = await chatEngine.chat({
+  message: "Evaluate the hackathon entry and provide scores and remarks.",
+  chatHistory: messages,
+ });
+ console.log({
+  response,
+  serviceContext,
+  raw: response.response,
+  sourceNodes: JSON.stringify(response.sourceNodes),
+  firstNode: !!response.sourceNodes?.length && response.sourceNodes[0],
+ });
+ const usedEmbeddingIds = response.sourceNodes?.map(node => node.id_) || [];
+ const parsedResponse = JSON.parse(response.response);
+ const evaluation = {
+  coherenceScore: parsedResponse.coherenceScore,
+  feasibilityScore: parsedResponse.feasibilityScore,
+  innovationScore: parsedResponse.innovationScore,
+  funScore: parsedResponse.funScore,
+  evaluationRemarks: parsedResponse.evaluationRemarks,
+ };
 
-
-    const response = await chatEngine.chat({
-        message: "Evaluate the hackathon entry and provide scores and remarks.",
-        chatHistory: messages,
-    });
-
-    const rawOutput = response.response;
-    return rawOutput;
+ const rawOutput = response.response;
+ return { enhancedProposal: rawOutput, messages, response: parsedResponse, usedEmbeddingIds, evaluation };
 }
 
 // Example usage for POST handler or another part of your application
 export async function POST(request: Request) {
-    const hackathonApp = await request.json(); // Assuming the request body is properly formatted
-    console.log(hackathonApp);
-    const enhancedProposal = await generateHackathonProposal(hackathonApp);
-    console.log(enhancedProposal, "Enhanced Proposal");
+ try {
+  const hackathonApp = await request.json(); // Assuming the request body is properly formatted
+  console.log(hackathonApp);
+  const { enhancedProposal, usedEmbeddingIds, messages, response, evaluation } = await generateHackathonProposal(
+   hackathonApp,
+  );
+  console.log(enhancedProposal, "Enhanced Proposal");
 
+  // Proceed with storing the enhanced proposal in MongoDB or returning it in the response
+  //
+  const db = client.db("aiUniverse"); // Connect to the database
+  const hackCodex = db.collection("hackerUniverse"); //
+  // assumed input
+  // run this function asynchronously, do not block for it to finish
+  runLlamaAndStore(db, hackathonApp, enhancedProposal, usedEmbeddingIds, messages, response, evaluation);
 
-    // Proceed with storing the enhanced proposal in MongoDB or returning it in the response
-    //
-    const db = client.db("aiUniverse"); // Connect to the database
-    const hackCodex = db.collection('hackerUniverse'); // 
+  await hackCodex.updateOne(
+   {
+    id: hackathonApp.projectId,
+    address: hackathonApp.address,
+    hack: hackathonApp.hack,
+   },
+   {
+    $addToSet: {
+     eval: enhancedProposal,
+    },
+   },
+   { upsert: true }, // this creates new document if none match the filter
+  );
 
-    // assumed input
-
-    llamaindex(JSON.stringify(enhancedProposal + hackathonApp), hackathonApp.projectId);//should we modify this id?
-
-    await hackCodex.updateOne(
-        {
-            _id: hackathonApp.projectId,
-            address: hackathonApp.address,
-            hack: hackathonApp.hack
-        },
-        {
-            $addToSet: {
-                eval: enhancedProposal,
-            }
-        },
-        { upsert: true },// this creates new document if none match the filter
-    );
-
-    // Implementation depends on application requirements.
-    //
-    return NextResponse.json(enhancedProposal);
+  // Implementation depends on application requirements.
+  //
+  return NextResponse.json(enhancedProposal);
+ } catch (e: any) {
+  console.error(e);
+  return NextResponse.json({ error: e.message || "An error occurred processing the request" }, { status: 500 });
+  // Handle error
+  //
+ }
 }
